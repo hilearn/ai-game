58534ae8fe3cba545bd62e3faf3129f8a234b327
SevakAbr <51233545+SevakAbr@users.noreply.github.com>
2021-10-01 15:19:27 +0400

    Pygame input (#3)
    * add keyboard listener

--- game/player.py
+++ game/player.py
@@ -1,3 +1,5 @@
+import pygame
+import threading
 from .gameobject import Player, Action, Stats
 
 
@@ -12,17 +14,30 @@ class RemotePlayer(Player):
 class KeyboardPlayer(Player):
     def __init__(self, stats: Stats):
         super().__init__(stats)
-        self.key_pressed = None
+        self.key_pressed = [Action.NOTHING, Action.NOTHING]
+        pygame.init()
+        listener = threading.Thread(target=self.listener, daemon=True)
+        listener.start()
 
     def decide(self):
-        if self.key_pressed is not None:
-            return self.key_pressed
-        else:
-            return Action.NOTHING
+        keys = self.key_pressed
+        self.key_pressed = [Action.NOTHING, Action.NOTHING]
+        return keys
 
     def listener(self):
         while True:
-            self.key_pressed = self.keyboard_listener.getkey()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.key_pressed[0] = Action.MOVE_LEFT
+            elif keys[pygame.K_RIGHT]:
+                self.key_pressed[0] = Action.MOVE_RIGHT
+            elif keys[pygame.K_DOWN]:
+                self.key_pressed[0] = Action.MOVE_DOWN
+            elif keys[pygame.K_UP]:
+                self.key_pressed[0] = Action.MOVE_UP
+            elif keys[pygame.K_SPACE]:
+                self.key_pressed[1] = Action.SHOOT
+            # pygame.event.pump()
 
 
 class Bot(Player):
